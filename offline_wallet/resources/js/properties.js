12e9af55fb43b187a94505176399dc8289f995fe
Rohit Chhabra <rohitchhh@gmail.com>
2017-09-01 19:46:06 +0530

    Added offline wallet code.

---
+++ offline_wallet/resources/js/properties.js
@@ -0,0 +1,6 @@
+var properties={
+	node_server_url:"http://ec2-18-221-53-138.us-east-2.compute.amazonaws.com:8545",
+	crowd_fund_url:"http://ec2-18-221-53-138.us-east-2.compute.amazonaws.com:8085/ammbr",
+	contract_address:"0x88CA06F45202F6760EfC1D65c4D91638ee1618a6",
+	download_wallet_url:"http://ec2-18-221-53-138.us-east-2.compute.amazonaws.com:8085/ammbr/download"
+}
